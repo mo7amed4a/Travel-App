1ebebad4aa6437f4e8268ea5695aef12d102392b
mo7amed4a <m7md0a.work@gmail.com>
2024-10-02 13:10:46 +0300

    add formik to sginup page

--- src/pages/auth/ForgotPassword.jsx
+++ src/pages/auth/ForgotPassword.jsx
@@ -1,52 +1,71 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { useFormik } from 'formik';
-import * as Yup from 'yup';
+import React from "react";
+import { Link } from "react-router-dom";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 export default function ForgotPasswordPage() {
-  // Define validation schema using Yup
   const validationSchema = Yup.object({
-    username: Yup.string()
-      .required('User Name is required'),
+    email: Yup.string()
+      .email("Invalid email address")
+      .required("Email is required"),
   });
 
-  // Initialize Formik
   const formik = useFormik({
     initialValues: {
-      username: '',
+      email: "",
     },
     validationSchema: validationSchema,
     onSubmit: (values) => {
-      console.log('Form data', values);
+      console.log("Form data", values);
       // Handle form submission, e.g., call your API here
     },
   });
 
   return (
-    <div className="h-screen w-screen flex justify-center items-center" style={{ backgroundImage: "url(/images/admin/bg.jpg)" }}>
+    <div
+      className="h-screen w-screen flex justify-center items-center"
+      style={{ backgroundImage: "url(/images/admin/bg.jpg)" }}
+    >
       <div className="shadow-2xl rounded bg-white p-4 w-full mx-4 md:w-3/4 lg:w-2/4 xl:w-1/4">
-        <form className="flex flex-col space-y-5" onSubmit={formik.handleSubmit}>
+        <form
+          className="flex flex-col space-y-5"
+          onSubmit={formik.handleSubmit}
+        >
           <h1 className="flex justify-center py-5">
             <a href="#">
               <img src="/images/admin/logo.png" alt="" />
             </a>
           </h1>
           <div className="flex flex-col">
-            <label htmlFor="username" className='text-sm pb-1 text-gray-700'>User Name</label>
+            <label
+              htmlFor="username"
+              className="text-sm pb-1 text-gray-700"
+            >
+              Email
+            </label>
             <input
-              id="username"
+              id="email"
               type="text"
-              {...formik.getFieldProps('username')}
-              className={`border ${formik.touched.username && formik.errors.username ? 'border-red-500' : 'border-gray-300'} rounded p-2`}
+              {...formik.getFieldProps("email")}
+              className={`border ${
+                formik.touched.email && formik.errors.email
+                  ? "border-red-500"
+                  : "border-gray-300"
+              } rounded p-2`}
             />
-            {formik.touched.username && formik.errors.username ? (
-              <div className="text-red-500 text-sm">{formik.errors.username}</div>
+            {formik.touched.email && formik.errors.email ? (
+              <div className="text-red-500 text-sm">{formik.errors.email}</div>
             ) : null}
           </div>
           <div className="w-full pt-2 flex justify-center">
-            <button type='submit' className="py-3 px-4 bg-secondary w-full text-center text-white font-semibold hover:bg-secondary/90 duration-100">Submit</button>
+            <button
+              type="submit"
+              className="py-3 px-4 bg-secondary w-full text-center text-white font-semibold hover:bg-secondary/90 duration-100"
+            >
+              Submit
+            </button>
           </div>
-          <div className='flex justify-between w-full text-secondary'>
+          <div className="flex justify-between w-full text-secondary">
             <Link to={"/auth/login"}>Do you want to login?</Link>
             <Link to="/auth/sginup">You don't have an account?</Link>
           </div>
@@ -55,4 +74,3 @@ export default function ForgotPasswordPage() {
     </div>
   );
 }
-
--- src/pages/auth/sginup.jsx
+++ src/pages/auth/sginup.jsx
@@ -1,41 +1,145 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { useFormik } from "formik";
+import React from "react";
+import { Link } from "react-router-dom";
+import * as Yup from "yup";
 
 export default function SginUpPage() {
+  const validationSchema = Yup.object({
+    name: Yup.string().required("Name is required"),
+    email: Yup.string()
+      .required("Email is required")
+      .email("Invalid email address"),
+    password: Yup.string()
+      .required("Password is required")
+      .min(6, "Password must be at least 6 characters"),
+    confirmPassword: Yup.string()
+      .required("Confirm password is required")
+      .min(6, "Comfort password must be at least 6 characters")
+      .oneOf([Yup.ref("password"), null], "Passwords must match"),
+  });
+
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+    validationSchema: validationSchema,
+
+    onSubmit: (values) => {
+      console.log("Form data", values);
+      // Handle form submission, e.g., call your API here
+    },
+  });
   return (
-    <div className="h-screen w-screen flex justify-center items-center" style={{backgroundImage: "url(/images/admin/bg.jpg)"}}>
-        <div className="shadow-2xl rounded bg-white p-4 w-full mx-4 md:w-3/4 lg:w-2/4 xl:w-1/4">
-            <form className="flex flex-col space-y-5">
-                <h1 className="flex justify-center py-5">
-                    <a href="#">
-                        <img src="/images/admin/logo.png" alt="" />
-                    </a>
-                </h1>
-                <div className="flex flex-col ">
-                    <label for="first_name1" className='text-sm pb-1 text-gray-700'>User Name</label>
-                    <input id='first_name1' type="text" />
-                </div>
-                <div className="flex flex-col ">
-                    <label for="first_name1" className='text-sm pb-1 text-gray-700'>Email</label>
-                    <input id='first_name1' type="email" />
-                </div>
-                <div className="flex flex-col ">
-                    <label for="last_name" className='text-sm pb-1 text-gray-700'>Password</label>
-                    <input id="last_name" type="password"/>
-                </div>
-                <div className="flex flex-col ">
-                    <label for="last_name" className='text-sm pb-1 text-gray-700'>Comfort Password</label>
-                    <input id="last_name" type="password"/>
-                </div>
-                <div className="w-full pt-2 flex justify-center">
-                    <button type="submit" className="py-3 px-4 bg-secondary w-full text-center text-white font-semibold hover:bg-secondary/90 duration-100" >Sgin Up</button>
-                </div>
-                <div className='flex justify-between w-full text-secondary'>
-                    <Link to="/auth/forgot-password">Forgot Password?</Link>
-                    <Link to={"/auth/login"}>Do you want to login?</Link>
-                </div>
-            </form>
-        </div>
+    <div
+      className="h-screen w-screen flex justify-center items-center"
+      style={{ backgroundImage: "url(/images/admin/bg.jpg)" }}
+    >
+      <div className="shadow-2xl rounded bg-white p-4 w-full mx-4 md:w-3/4 lg:w-2/4 xl:w-1/4">
+        <form
+          className="flex flex-col space-y-5"
+          onSubmit={formik.handleSubmit}
+        >
+          <h1 className="flex justify-center py-5">
+            <a href="#">
+              <img src="/images/admin/logo.png" alt="" />
+            </a>
+          </h1>
+          <div className="flex flex-col ">
+            <label htmlFor="name" className="text-sm pb-1 text-gray-700">
+              Name
+            </label>
+            <input
+              id="name"
+              type="text"
+              {...formik.getFieldProps("name")}
+              className={`border ${
+                formik.touched.name && formik.errors.name
+                  ? "border-red-500"
+                  : "border-gray-300"
+              } rounded p-2`}
+            />
+            {formik.touched.name && formik.errors.name ? (
+              <div className="text-red-500 text-sm">{formik.errors.name}</div>
+            ) : null}
+          </div>
+          <div className="flex flex-col ">
+            <label htmlFor="email" className="text-sm pb-1 text-gray-700">
+              Email
+            </label>
+            <input
+              id="email"
+              type="email"
+              {...formik.getFieldProps("email")}
+              className={`border ${
+                formik.touched.email && formik.errors.email
+                  ? "border-red-500"
+                  : "border-gray-300"
+              } rounded p-2`}
+            />
+            {formik.touched.email && formik.errors.email ? (
+              <div className="text-red-500 text-sm">{formik.errors.email}</div>
+            ) : null}
+          </div>
+          <div className="flex flex-col ">
+            <label htmlFor="password" className="text-sm pb-1 text-gray-700">
+              Password
+            </label>
+            <input
+              id="password"
+              type="password"
+              {...formik.getFieldProps("password")}
+              className={`border ${
+                formik.touched.password && formik.errors.password
+                  ? "border-red-500"
+                  : "border-gray-300"
+              } rounded p-2`}
+            />
+            {formik.touched.password && formik.errors.password ? (
+              <div className="text-red-500 text-sm">
+                {formik.errors.password}
+              </div>
+            ) : null}
+          </div>
+          <div className="flex flex-col ">
+            <label
+              htmlFor="confirmPassword"
+              className="text-sm pb-1 text-gray-700"
+            >
+              Confirm Password
+            </label>
+            <input
+              id="confirmPassword"
+              type="password"
+              {...formik.getFieldProps("confirmPassword")}
+              className={`border ${
+                formik.touched.confirmPassword && formik.errors.confirmPassword
+                  ? "border-red-500"
+                  : "border-gray-300"
+              } rounded p-2`}
+            />
+            {formik.touched.confirmPassword && formik.errors.confirmPassword ? (
+              <div className="text-red-500 text-sm">
+                {formik.errors.confirmPassword}
+              </div>
+            ) : null}
+          </div>
+          <div className="w-full pt-2 flex justify-center">
+            <button
+              type="submit"
+              className="py-3 px-4 bg-secondary w-full text-center text-white font-semibold hover:bg-secondary/90 duration-100"
+            >
+              Sgin Up
+            </button>
+          </div>
+          <div className="flex justify-between w-full text-secondary">
+            <Link to="/auth/forgot-password">Forgot Password?</Link>
+            <Link to={"/auth/login"}>Do you want to login?</Link>
+          </div>
+        </form>
+      </div>
     </div>
-  )
+  );
 }
